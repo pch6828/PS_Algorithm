323b1c5cc896cc84fe8045cd526b8e83a4b76855
pch6828 <pch6828@hanyang.ac.kr>
2022-03-19 22:12:00 +0900

    3/19 : solved one bronze problem

---
+++ BOJ/BOJ 20499 Darius님 한타안함.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <list>
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+using namespace std;
+
+#define LIMIT 10000000000000000
+#define MOD 1000000007
+#define PI 3.14159265359
+#define SIZE 1000
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int k, d, a;
+
+int main(void)
+{
+    scanf("%d/%d/%d", &k, &d, &a);
+
+    printf((k + a < d || d == 0) ? "hasu" : "gosu");
+}
