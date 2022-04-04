30bc6bb7c3e642dcc5d7d28664e906820ec63c57
pch6828 <pch6828@hanyang.ac.kr>
2022-04-04 16:00:16 +0900

    4/4 : solved one gold problem

---
+++ BOJ/BOJ 20500 Ezreal 여눈부터 가네 ㅈㅈ.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <list>
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+using namespace std;
+
+#define LIMIT 10000000000000000
+#define MOD 1000000007
+#define PI 3.14159265359
+#define SIZE 1000
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+typedef vector<int> LargeNum;
+
+int dp[1516][3], n;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+
+    dp[2][0] = 1;
+    dp[2][1] = 1;
+
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % MOD;
+        dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+        dp[i][2] = (dp[i - 1][1] + dp[i - 1][0]) % MOD;
+    }
+
+    cout << dp[n][0];
+}
