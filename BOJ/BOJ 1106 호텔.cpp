270f06b2eb12aed12aac8d6ad12788606df549e3
pch6828 <pch6828@hanyang.ac.kr>
2022-04-05 17:02:07 +0900

    4/5 : solved one silver problem

---
+++ BOJ/BOJ 1106 호텔.cpp
@@ -0,0 +1,70 @@
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <list>
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+using namespace std;
+
+#define LIMIT 10000000000000000
+#define MOD 1000000007
+#define PI 3.14159265359
+#define SIZE 1000
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+typedef vector<int> LargeNum;
+
+int c, n, dp[1501], ans;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> c >> n;
+
+    for (int i = 1; i <= 1500; i++)
+    {
+        dp[i] = MOD;
+    }
+
+    while (n--)
+    {
+        int cost, customer;
+        cin >> cost >> customer;
+
+        for (int i = 0; i < c; i++)
+        {
+            dp[i + customer] = min(dp[i + customer], dp[i] + cost);
+        }
+    }
+
+    int ans = dp[c];
+    for (int i = c; i <= 1500; i++)
+    {
+        ans = min(dp[i], ans);
+    }
+    cout << ans;
+}
