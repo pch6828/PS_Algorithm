0e7672d82ec508508d451bee7cd2456398aa2204
pch6828 <pch6828@hanyang.ac.kr>
2022-05-20 15:44:27 +0900

    5/20 : solved one bronze problem

---
+++ BOJ/BOJ 1592 영식이와 친구들.cpp
@@ -0,0 +1,65 @@
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <list>
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+using namespace std;
+
+#define LIMIT 10000000000000000
+#define MOD 1000000007
+#define PI 3.14159265359
+#define SIZE 1000
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+typedef vector<int> LargeNum;
+
+int n, m, l, ans;
+int cnt[50];
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m >> l;
+    cnt[0] = 1;
+    int now = 0;
+
+    while (cnt[now] != m)
+    {
+        if (cnt[now] % 2)
+        {
+            now = (now + l) % n;
+        }
+        else
+        {
+            now = (now - l + n) % n;
+        }
+        cnt[now]++;
+        ans++;
+    }
+    cout << ans;
+}
